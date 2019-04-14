39b1aa5687dcf745be274792b74094f382796084
Novack <mnovack@udel.edu>
2019-04-14 11:30:18 -0400

    Part 1 of GUI tutorial

--- MineSweeperAssignment1/MineSweeper/MineSweeper.cpp
+++ MineSweeperAssignment1/MineSweeper/MineSweeper.cpp
@@ -1,15 +1,14 @@
-// MineSweeper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
 #include "HelloWorld.h"
 #include "HelloWorldClass.h"
 using namespace std;
 
+
 string samplePrint() {
 	return "Hello World 2!";
 }
-
+/*
 int main()
 {
 	cout << "Hello World!" << endl;;
@@ -24,7 +23,103 @@ int main()
 	delete& board;
 
 }
+*/
 
+#define FILE_MENU_NEW 1
+#define FILE_MENU_OPEN 2
+#define FILE_MENU_EXIT 3
+#define CHANGE_TITLE 4
+
+HMENU hMenu;
+HWND hEdit;
+void AddMenus(HWND hWnd);
+void AddControls(HWND hWnd);
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPreventInst, LPSTR args, int ncmdshow) {
+
+	WNDCLASSW wc = { 0 };
+
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hInstance = hInst;
+	wc.lpszClassName = L"myWindowClass";
+	wc.lpfnWndProc = WindowProcedure;
+
+	if (!RegisterClassW(&wc))
+		return -1;
+
+	CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+	MSG msg = { 0 };
+
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	//MessageBox(NULL, L"Sample Box", L"Sub text", MB_OK);
+	return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg)
+	{
+	case WM_COMMAND:
+		switch (wp) 
+		{
+		case FILE_MENU_NEW:
+			MessageBeep(MB_OK);
+			break;
+		case FILE_MENU_OPEN:
+			MessageBeep(MB_OK);
+			break;
+		case FILE_MENU_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case CHANGE_TITLE:
+			wchar_t text[100];
+			GetWindowTextW(hEdit, text, 100);
+			SetWindowTextW(hWnd, text);
+			break;
+		}
+		break;
+	case WM_CREATE:
+		AddMenus(hWnd);
+		AddControls(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+}
+
+void AddMenus(HWND hWnd) {
+	hMenu = CreateMenu();
+	HMENU hFileMenu = CreateMenu();
+	HMENU hSubMenu = CreateMenu();
+
+	AppendMenu(hSubMenu, MF_STRING, CHANGE_TITLE, L"Change Title");
+
+	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, L"New");
+	AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, L"Open SubMent");
+	AppendMenu(hFileMenu, MF_SEPARATOR, NULL, NULL);
+	AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, L"Exit");
+
+	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, L"File");
+	
+
+	SetMenu(hWnd, hMenu);
+}
+
+void AddControls(HWND hWnd) {
+	CreateWindowW(L"static", L"Enter text here :", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 200, 100, 100, 50, hWnd, NULL, NULL, NULL);
+	hEdit = CreateWindowW(L"edit", L"...", WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOVSCROLL, 200, 152, 100, 50, hWnd, NULL, NULL, NULL);
+}
 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
